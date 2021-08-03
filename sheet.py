7d30394b9af3bc5794eb9aa863abaac2963b3cec
vladimir <grun_gespenst@tut.by>
2021-08-03 22:10:07 +0300

    sheet_complite

--- sheet.py
+++ sheet.py
@@ -1,21 +1,21 @@
 def counter():
-    lenght = float(input("Введите дину, мм:"))
+    lenght = float(input("Введите динну, мм:"))
     weght = float(input("Введите ширину, мм:"))
     squear = lenght * weght
     return squear
 
-th = float(input('Введите толщину листа:'))
+th = float(input('Введите толщину листа, мм:'))
 stek_sq = []
 stek_masa = []
 while True:
     sq = counter() / 1000000
     stek_sq.append(sq)
-    masa = sq * 7850
+    masa = sq * th * 7.850
     stek_masa.append(masa)
-    again = str(input('Добавить еще лист?'))
-    if again == 'Y' or again == 'Д' or again == 'Yes' or again == 'Да':
+    again = str(input('Добавить еще лист?(Да, Yes/No, Нет):'))
+    if again == 'Y'.lower() or again == 'Д'.lower() or again == 'Yes'.lower() or again == 'Да'.lower():
         continue
     break
 
-print('Оющая площадь, метров квадратных:', round(sum(stek_sq)), 2)
-print('Общая масса, кг:', round(sum(stek_masa)), 2)
+print('Общая площадь, метров квадратных:', sum(stek_sq))
+print('Общая масса, кг:', sum(stek_masa))
