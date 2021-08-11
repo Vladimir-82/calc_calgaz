5fbe7f07a714f847bf435d1c853c60841420f272
vladimir <grun_gespenst@tut.by>
2021-08-11 07:04:57 +0300

    complete

--- sheet.py
+++ sheet.py
@@ -1,18 +1,21 @@
 def counter():
-    lenght = float(input("Введите динну, мм:"))
+    lenght = float(input("Введите длину, мм:"))
     weght = float(input("Введите ширину, мм:"))
     squear = lenght * weght
     return squear
 
-th = float(input('Введите толщину листа, мм:'))
+
+
 stek_sq = []
 stek_masa = []
 while True:
-    sq = counter() / 1000000
+    th = float(input('Введите толщину листа, мм:'))
+    quontity = int(input('Введите колличество деталей:'))
+    sq = quontity * counter() / 1000000
     stek_sq.append(sq)
     masa = sq * th * 7.850
     stek_masa.append(masa)
-    again = str(input('Добавить еще лист?(Да, Yes/No, Нет):'))
+    again = str(input('Добавить еще лист?(Да (Д), Yes(Y)/No, Нет):'))
     if again == 'Y'.lower() or again == 'Д'.lower() or again == 'Yes'.lower() or again == 'Да'.lower():
         continue
     break
